ce33953434976c8b988772cab705e6f471041aff
vanlinhveriserve <109492526+vanlinhveriserve@users.noreply.github.com>
2023-01-27 15:10:40 +0700

    add component tranfer data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentTestComponent } from './component-test/component-test.component';
 import { BindEventComponent } from './bind-event/bind-event.component';
+import { FormsModule } from '@angular/forms';
+import { FormComponent } from './form/form.component';
+import { TranferDataComponent } from './tranfer-data/tranfer-data.component';
+import { TranferDataChild2ParentComponent } from './tranfer-data-child2-parent/tranfer-data-child2-parent.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ComponentTestComponent,
-    BindEventComponent
+    BindEventComponent,
+    FormComponent,
+    TranferDataComponent,
+    TranferDataChild2ParentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
